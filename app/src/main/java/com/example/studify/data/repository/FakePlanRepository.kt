ecc1fc08784ccc68a64b6f59ed624bfc1bfc32a7
나형진 <skgudwls@konkuk.ac.kr>
2025-06-10 12:43:11 +0900

    feat(plan): basic Plan list UI and ViewModel

--- app/src/main/java/com/example/studify/data/repository/FakePlanRepository.kt
+++ app/src/main/java/com/example/studify/data/repository/FakePlanRepository.kt
@@ -4,7 +4,10 @@ import com.example.studify.data.local.dao.PlanDao
 import com.example.studify.data.local.entity.StudyPlanEntity
 import com.example.studify.domain.repository.PlanRepository
 import com.example.studify.domain.repository.SubjectInput
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -14,6 +17,13 @@ class FakePlanRepository
     constructor(
         private val dao: PlanDao
     ) : PlanRepository {
+        init {
+            CoroutineScope(Dispatchers.IO).launch { // or CoroutineScope(Dispatchers.IO)
+                dao.upsert(StudyPlanEntity())
+                dao.upsert(StudyPlanEntity())
+            }
+        }
+
         override fun observePlans(): Flow<List<StudyPlanEntity>> = dao.observePlans()
 
         override suspend fun createPlanWithLLM(inputs: List<SubjectInput>) {
